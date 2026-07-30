9a23cf4a7f470f1d4139b43a97cfb9b94ff56a76
Lucas Fidalgo <297744658+lucas-fidalgo@users.noreply.github.com>
2026-07-29 23:36:05 -0300

    Add Exercise 1.12 solution

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => <div><button onClick={onClick}>{text}</button></div>  
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const numOfAnecdotes = anecdotes.length
+  const randomIndex = Math.floor(Math.random() * numOfAnecdotes)
+  const [selected, setSelected] = useState(randomIndex)
+
+  return(
+    <div>
+      {anecdotes[selected]}
+      <Button onClick={() => 
+                      setSelected((selected+1) >= numOfAnecdotes ? 0 : (selected+1))}
+              text="next anecdote" />
+    </div>
+  )
+}
+
+export default App
