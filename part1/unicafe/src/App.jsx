d3a68adc48bf7a4d866ddc3a60dc716547fe7302
Lucas Fidalgo <297744658+lucas-fidalgo@users.noreply.github.com>
2026-07-29 22:24:04 -0300

    Add Exercise 1.7 solution

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 
-const Display = ({ text, value }) =>
-  <p>{text} {value}</p>
-
+const Display = (props) => {
+  if (props.percent) {
+    return(
+      <p>{props.text} {props.value} %</p>
+    )
+  }
+  return(
+    <p>{props.text} {props.value}</p>
+  )
+}
 const Button = ({ onClick, text }) =>
   <button onClick={onClick}>{text}</button>
 
@@ -10,18 +17,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const calculateStats = (g, n, b) => {
+    const a = g + b + n
+
+    setAll(a);
+    setAverage((g - b) / a)
+    setPositive((g / a) * 100)
+  }
+
+  const addGoodReview = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    calculateStats(updatedGood, neutral, bad)
+  }
+
+  const addNeutralReview = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    calculateStats(good, updatedNeutral, bad)
+  }
+
+  const addBadReview = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    calculateStats(good, neutral, updatedBad)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={() => setGood(good + 1) } text='good' />
-      <Button onClick={() => setNeutral(neutral + 1) } text='neutral' />
-      <Button onClick={() => setBad(bad + 1) } text='bad' />
+      <Button onClick={addGoodReview} text='good' />
+      <Button onClick={addNeutralReview} text='neutral' />
+      <Button onClick={addBadReview} text='bad' />
 
       <h1>statistics</h1>
       <Display text='good' value={good} />
       <Display text='neutral' value={neutral} />
       <Display text='bad' value={bad} />
+      <Display text='all' value={all} />
+      <Display text='average' value={average} />
+      <Display text='positive' value={positive} percent="true" />
     </div>
   )
 }
